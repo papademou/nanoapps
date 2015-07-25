a9ca7139daeec6315cdd639410243bd29ff53291
papademou <papaedemou@gmail.com>
2015-07-24 22:26:21 -0400

    use xml onclick attribute for buttons + cleanup

--- Portfolio/app/src/main/java/com/papademou/portfolio/MainActivity.java
+++ Portfolio/app/src/main/java/com/papademou/portfolio/MainActivity.java
@@ -18,42 +18,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //bind click action to all buttons
-        LinearLayout btn_wrapper = (LinearLayout) findViewById(R.id.layout_btn_wrapper);
-        for (int i = 0; i <= btn_wrapper.getChildCount(); i++) {
-            View v = btn_wrapper.getChildAt(i);
-            if (v instanceof Button) {
-                v.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        displayButtonPurpose(view);
-                    }
-                });
-            }
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**

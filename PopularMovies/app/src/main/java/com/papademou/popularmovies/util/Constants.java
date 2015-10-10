f71f15ed7e7b3d5d37d5a24a61828c50bc5f4878
papademou <papaedemou@gmail.com>
2015-10-09 22:59:09 -0400

    Remove dev key

--- PopularMovies/app/src/main/java/com/papademou/popularmovies/util/Constants.java
+++ PopularMovies/app/src/main/java/com/papademou/popularmovies/util/Constants.java
@@ -5,7 +5,7 @@ package com.papademou.popularmovies.util;
  */
 public final class Constants {
     public static final String TMDB_BASE_URL = "http://api.themoviedb.org";
-    public static final String TMDB_API_KEY = "8177a889cc5bfcc7b1ca7dc857932a53"; //The Movie DB API Key
+    public static final String TMDB_API_KEY = "Enter your API Key here"; //The Movie DB API Key
     public static final String TMDB_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185";
     public static final String YOUTUBE_THUMBNAIL_URI =
             "http://img.youtube.com/vi/{VIDEO_ID}/default.jpg";
